68bf2acfb67038c6209d31ad8bd3e147184dbefe
minhlexuan1909 <minhlx1909@gmail.com>
2023-02-21 02:02:34 +0700

    randomize production class name

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,24 @@
+import { StylesProvider } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  // Add "co" to start of any class name
+  productionPrefix: "co",
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,20 @@
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
+// Avoid random classes between microFE collide
+
+const generateClassName = createGenerateClassName({
+  // Add "ma" to start of any class name
+  productionPrefix: "ma",
+});
+
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
